75e35c1fadf40d907c25b3248b4f7e03374993ff
MFriend <Michael.Friend@freedompay.com>
2023-12-05 17:19:51 -0500

    WIP day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -20,7 +20,7 @@ class MapTable(ranges: List<Pair<LongRange, LongRange>>) {
     private val sortedRanges: SortedSet<Pair<LongRange, LongRange>> = ranges.toSortedSet(compareBy { it.first.first })
     fun lookUp(num: Long): Long {
         for ((sourceRang, destRang) in sortedRanges) {
-            if (num in sourceRang) return sourceRang.indexOf(num) + destRang.first
+            if (num in sourceRang) return (num - sourceRang.first) + destRang.first
             if (sourceRang.last > num) return num
         }
         return num
@@ -37,7 +37,7 @@ suspend fun <T, R> List<T>.mapAsync(op: suspend (T) -> R): List<R> {
     }
 }
 
-suspend fun main() {
+fun main() {
     fun processToMap(lines: List<String>): Pair<String, MapTable> {
         val (header, table) = lines.split()
         val title = header.uppercase().split(" ").first()
@@ -57,44 +57,43 @@ suspend fun main() {
             }.toMap()
     }
 
-    suspend fun part1(input: String): Long {
+    fun part1(input: String): Long {
         val (seeds, mappings) = input.split("\n\n").let {
             it.first().substringAfter(": ").trim().split(" ").map(String::toLong) to it.drop(1)
         }
 
         val lookups = mappings.associate { processToMap(it.lines()) }
 
-        return seeds.mapAsync {
+        return seeds.minOf{
             val number = Field.ordered.fold(it) { num, (from, to) ->
                 lookups.getOrValue(from mapTo to, num)
             }
             number
-        }.min()
+        }
     }
 
-    suspend fun part2(input: String): Long {
+    fun part2(input: String): Long {
         val (seeds, mappings) = input.split("\n\n").let {
             it.first().substringAfter(": ").trim().split(" ").map(String::toLong) to it.drop(1)
         }
 
         val lookups = mappings.associate { processToMap(it.lines()) }
-        val allSeeds = seeds.chunked(2).flatMap { (start, length) -> start..<start + length }.also { it.println() }
-        return allSeeds.mapAsync {
+        val allSeeds = seeds.chunked(2).flatMap { (start, length) -> start..<start + length }
+        return allSeeds.minOf {
             val number = Field.ordered.fold(it) { num, (from, to) ->
                 lookups.getOrValue(from mapTo to, num)
             }
-            if(it == 82.toLong()) println(number)
             number
-        }.min()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputAsString("Day05_test")
-    check(part1(testInput).also { it.println() } == 35.toLong())
+    check(part1(testInput) == 35.toLong())
 
     val input = readInputAsString("Day05")
     measureTime { part1(input).println() }.println()
-    check(part2(testInput).also { it.println() } == 46.toLong())
+    check(part2(testInput)== 46.toLong())
     measureTime { part2(input).println() }.println()
 
 //    part2(input).println()
