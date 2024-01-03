3f9e2ae895483cd7b9600822ac5df4049284e745
MFriend <Michael.Friend@freedompay.com>
2024-01-03 15:49:18 -0500

    Day13 p2

--- src/Day13.kt
+++ src/Day13.kt
@@ -32,17 +32,44 @@ fun main() {
         }.alsoPrint().sum()
     }
 
+    fun findHorizontalMirrorSmudge(map: List<String>) = (0..<map.size - 1).firstOrNull {
+        val above = map.slice(0..it)
+        val below = map.slice(it + 1..<map.size)
+        val checkSize = min(above.size, below.size)
+        val pairs =
+            above.takeLast(checkSize).reversed().zip(below.take(checkSize)).filter { (above, below) -> above != below }
+        val smudgedPair = pairs.singleOrNull()
+            ?.takeIf { (above, below) -> above.filterIndexed { index, c -> c != below[index] }.length == 1 }
+        smudgedPair != null
+    }?.let { it + 1 } ?: 0
+
+    fun findVerticalMirrorSmudge(map: List<String>) = (0..<map.first().length - 1).firstOrNull { index ->
+        println("slicing at $index")
+        val left = map.map { it.take(index + 1) }
+        val right = map.map { it.drop(index + 1) }
+        left.zip(right).map { (l, r) -> "$l|$r" }.alsoPrintLines()
+        val checkSize = min(left.first().length, right.first().length)
+        val checkLeft = left.map { it.takeLast(checkSize) }
+        val checkRight = right.map { it.take(checkSize).reversed() }
+        val badPair =
+            checkLeft.zip(checkRight).singleOrNull { (left, right) -> left != right } ?: return@firstOrNull false
+        badPair.first.filterIndexed { i, c -> c != badPair.second[i] }.singleOrNull() != null
+    }?.let { it + 1 } ?: 0
+
     fun part2(input: String): Int {
         val maps = parseInput(input)
-        return maps.size
+
+        return maps.map { map ->
+            (findHorizontalMirrorSmudge(map) * 100) + findVerticalMirrorSmudge(map)
+        }.alsoPrint().sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputAsString("Day13_test")
     checkEqual(405, part1(testInput))
-    // checkEqual(1, part2(testInput))
+    checkEqual(400, part2(testInput))
 
     val input = readInputAsString("Day13")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
